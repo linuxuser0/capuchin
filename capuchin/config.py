50c145be08f9f53d5abb1bce39656e881384e5b9
Linux User <sudormdashrfslash@gmail.com>
2014-01-26 18:33:20 -0800

    Update config.py

--- capuchin/config.py
+++ capuchin/config.py
@@ -6,7 +6,7 @@ FEED_LOCATION = realpath("test/feed")
 SORTED_LOCATION = realpath("test/sorted")
 SORTED_IMAGE_LOCATION = realpath("test/sorted_corpus")
 INITIAL_LOCATION = realpath("test/initial")
-IMAGE_PACKAGE_SIZE = 5
+IMAGE_PACKAGE_SIZE = 30 
 NUM_PROTOTYPES = 10
 MAX_TRIES = 4 
 
